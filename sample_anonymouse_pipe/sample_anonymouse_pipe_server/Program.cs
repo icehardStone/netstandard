386d3fe620de5426c916ce84304ecd35113fbb5e
胡光华 <huguanghua@huhangmed.com>
2020-02-18 09:49:40 +0800

    buffer 和 匿名管道练习

---
+++ sample_anonymouse_pipe/sample_anonymouse_pipe_server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace sample_anonymouse_pipe_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Process  client = new Process();
+            client.StartInfo.FileName="sample_anonymouse_pipe_client";
+            using(AnonymousPipeServerStream server = new AnonymousPipeServerStream(PipeDirection.Out,HandleInheritability.Inheritable))
+            {
+                Console.WriteLine("[Server Curren TransmissionMode:{0}]",server.TransmissionMode);
+
+                client.StartInfo.Arguments = server.GetClientHandleAsString();
+                client.StartInfo.UseShellExecute = false;
+                client.Start();
+
+                server.DisposeLocalCopyOfClientHandle();
+
+                try{
+                    using(StreamWriter writer = new StreamWriter(server))
+                    {
+                        writer.AutoFlush = true;
+                        writer.WriteLine("SYNC");
+
+                        server.WaitForPipeDrain();
+                        Console.WriteLine("[Server] Enter text");
+                        writer.WriteLine(Console.ReadLine());
+                    }
+
+                }
+                catch(IOException error)
+                {
+                    Console.WriteLine(error);
+                }
+            }
+        }
+    }
+}
