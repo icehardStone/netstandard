b5f53692c769a3ca3286c1000c1cffa6beb3fb36
胡光华 <huguanghua@huhangmed.com>
2020-03-19 23:11:13 +0800

    博客项目

---
+++ toolClass/Src/MicroService.Tools/QueryPage.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MicroService.Tools
+{
+    public static class QueryPage
+    {
+        public static IQueryable<T> SplitePage<T>(this IQueryable<T> db, )  where T : class
+        {
+
+        } 
+    }
+}
